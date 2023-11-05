02701a06e6b552d007f0f05aab4c0491b20bdb24
Victor-Pafaro <vitor.pafaro@gmail.com>
2023-11-05 02:30:56 -0300

    Implementação do chatbot

--- bin/views.py
+++ bin/views.py
@@ -4,6 +4,7 @@ from .forms import CriarContaForm, FormHomebin
 # Create your views here.
 from django.views.generic import TemplateView, ListView, DetailView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .chat import Chat
 
 
 # def login(request):
@@ -20,14 +21,34 @@ class Homebin(LoginRequiredMixin,FormView):
     template_name = "homebin.html"
     form_class= FormHomebin
 
-    def get_success_url(self):
-        pergunta = self.request.POST.get("pergunta") # Pega a pergunta
-        perguntas = Pergunta.objects.filter(descricao_pergunta=pergunta) #Verifica se a pergunta feita é a mesma que tem no campo de descrição pergunta no banco de dados.
-        # Fazer a parte da API aqui, por enquanto se a pergunta feita for igual a descrição pergunta que tem no banco vai levar a pessoa pra pagina de login.
-        if perguntas:
-            return reverse('bin:perguntacerta')
-        else:
-            return reverse('bin:perguntaerrada')
+    def form_valid(self, form):
+        chat = Chat()
+        chat.treino()
+        # Acessar o valor do campo de entrada após a validação
+        valor_digitado = form.cleaned_data['pergunta']
+        chat.pergunta = valor_digitado
+
+        resposta = chat.get_resposta()
+        if(resposta == 'Empty Response'):
+            print('Desculpe essa pergunta não está em meu escopo de conhecimento')
+        print(resposta)
+        # return super().form_valid(form)
+    # def get_success_url(self):
+    #     return reverse('bin:login')
+
+    # def get_success_url(self):
+        # Redirecionar para a URL de sucesso
+        #
+        # return reverse('bin:perguntacerta')
+
+    # def get_success_url(self):
+    #     pergunta = self.request.POST.get("pergunta") # Pega a pergunta
+    #     perguntas = Pergunta.objects.filter(descricao_pergunta=pergunta) #Verifica se a pergunta feita é a mesma que tem no campo de descrição pergunta no banco de dados.
+    #     # Fazer a parte da API aqui, por enquanto se a pergunta feita for igual a descrição pergunta que tem no banco vai levar a pessoa pra pagina de login.
+    #     if perguntas:
+    #         return reverse('bin:perguntacerta')
+    #     else:
+    #         return reverse('bin:perguntaerrada')
 
 
 class Cadastro(FormView):
