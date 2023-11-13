3af54cbe6e28d61071f2566cfd45d9be467f090e
mcsansp <miguel.fatec.cc@gmail.com>
2023-11-13 18:43:41 -0300

    ultima atualização

--- bin/chat.py
+++ bin/chat.py
@@ -12,7 +12,7 @@ class Chat:
         return response
 
     def treino(self):
-        os.environ["OPENAI_API_KEY"] = "token-aqui"
+        os.environ["OPENAI_API_KEY"] = "chave-token-aqui"
 
         path = os.path.join(os.path.abspath("."),"bin", "artigos")
         print(path)
